b4d96962d7186060e9bb4ebf8b7806edb5aece02
Austen Haymond <ahaymon17@gmail.com>
2020-03-21 23:28:24 -0700

    fixed html route typo

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,6 +3,6 @@ const path = require("path");
 module.exports = function(app) {
 
     app.get("/", function(req, res) {
-        res.sendDile(path.join(__dirname, "../public/home.html"));
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 }
