140fe206b68ac90a05e4cb4484c28dc8a5e98b0a
Austen Haymond <ahaymon17@gmail.com>
2020-03-22 18:02:35 -0700

    added nav bar, html pages and routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,4 +5,13 @@ module.exports = function(app) {
     app.get("/", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
+    app.get("/aboutme", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/aboutMe.html"));
+    });
+    app.get("/projects", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/projects.html"));
+    });
+    app.get("/contact", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/contact.html"));
+    });
 }
